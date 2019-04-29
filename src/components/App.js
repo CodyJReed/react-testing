0e4cb353767d7828629cb127b97c397f8285861f
Cody Reed <cody2j2reed@gmail.com>
2019-04-28 22:06:49 -0400

    Refactor to class, add connect and changeAuth.  Created two helper functions

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,47 @@
 import React from "react"
-import { Route } from "react-router-dom"
+import { Route, Link } from "react-router-dom"
+import { connect } from "react-redux"
 
 import CommentList from "./CommentList"
 import CommentBox from "./CommentBox"
+import {changeAuth} from "../actions"
 
-const App = () => {
-    return (
-        <div>
-            <Route path="/post" component={CommentBox}/>
-            <Route exact path="/" component={CommentList} />
-        </div>
-    )
+class App extends React.Component {
+    renderButton() {
+        if (this.props.auth) {
+            return <button onClick={() => this.props.changeAuth(false)}>Sign Out</button>
+        } else {
+            return <button onClick={() => this.props.changeAuth(true)}>Sign In</button>
+        }
+    }
+
+    renderHeader() {
+        return (
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/post">Post a Comment</Link>
+              </li>
+              <li>
+                {this.renderButton()}
+              </li>
+            </ul>
+        )
+    }
+
+    render(){
+        return (
+            <div>
+                {this.renderHeader()}
+                <Route path="/post" component={CommentBox}/>
+                <Route exact path="/" component={CommentList} />
+            </div>
+        )
+    }
 }
-export default App
+
+const mapStateToProps = ({auth}) => ({auth})
+
+export default connect(mapStateToProps, { changeAuth })(App)
